8297a8c119e2b6081c04128c08ea2c720ab248d7
danielzigo <16023906+danielzigo@users.noreply.github.com>
2026-04-25 03:40:49 +0100

    feat: implement cocktails section plus animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
 
 // This registration is only needed once, so it's good to have it here.
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
     </main>
   )
 }
